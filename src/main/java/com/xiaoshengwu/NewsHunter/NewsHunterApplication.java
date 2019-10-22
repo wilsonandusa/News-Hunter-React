d7506249614f5c82b563bd8b575dbf99470c442e
WilsonWu <wilsonandiphone@icloud.com>
2019-10-21 20:56:18 -0700

    Initial commit

---
+++ src/main/java/com/xiaoshengwu/NewsHunter/NewsHunterApplication.java
@@ -0,0 +1,14 @@
+package com.xiaoshengwu.NewsHunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsHunterApplication.class, args);
+		System.out.println("News-Hunter-Started!");
+	}
+
+}
---
+++ src/test/java/com/xiaoshengwu/NewsHunter/NewsHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoshengwu.NewsHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
